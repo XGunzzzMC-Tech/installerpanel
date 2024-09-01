7c52fecc1e9e73d93d71b281418b5942566babf9
XGunzzzMC-Tech <xlxg1204@gmail.com>
2024-09-01 13:23:04 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,20 @@
+/*
+[+] =============================================== [+]
+[+]                                                            [+]
+[+] - INSTALLER PTERODACTYL                              [+]
+[+]                                                            [+]
+[+]  TQTO :                                                   [+]
+[+] • WannOffc ( MySelf)                                     [+]
+[+] • Creator Bot WhatsApp & Telegram                     [+]
+[+] • Para Pengguna Bot Tele & Wa                         [+]
+[+]                                                            [+]
+[+] © CreateByWannFyy                                      [+]
+[+] =============================================== [+]
+*/
+
+const config = {
+  token: '7224377857:AAGnKWx_X8U28ZDsAOzqjOQtxQ3ugznan9M',
+  adminId: '6890528297', 
+};
+
+module.exports = config;
